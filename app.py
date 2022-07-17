4d5381580d2f1fcb12de8b9d86ae8e3762e516ea
Dan Iozsa <diozsa@yahoo.com>
2022-07-16 19:14:59 -0700

    implemented Flask session to collect responses

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from crypt import methods
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from http.client import responses
+from flask import Flask, request, render_template, redirect, flash, session
 from random import randint, choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "programare"
+app.config['SECRET_KEY'] = "programming"
 
 #for Flask debug tool turn redirect prompt to off
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+#responses = []
 
 @app.route('/')
 def home_page():
@@ -20,12 +21,22 @@ def home_page():
     return render_template('home.html', survey=survey)
 
 
+@app.route('/start', methods=['POST'])
+def start_survey():
+    session["responses"] = []
+    return redirect ('/questions/0')
+
+
 @app.route('/answer', methods=['POST'])
 def get_answer():
     
     response = request.form['radio']
-    
+
+    responses = session["responses"]
     responses.append(response)
+    session["response"] = responses
+
+
     if len(responses) >= len(survey.questions):
         return redirect ('/completed')
     else:
@@ -34,6 +45,11 @@ def get_answer():
 
 @app.route('/questions/<int:index>')
 def show_question(index):
+    responses = session["responses"]
+
+    if responses == None:
+        return redirect('/')
+
 # if accessing questions out of order.
     if (len(responses) != index):
         flash(f"Accessing invalid question! FORBIDDEN!! ACHTUNG!! Continue with current question.")
@@ -50,5 +66,6 @@ def show_question(index):
 
 @app.route('/completed')
 def completed():
-    return render_template('/completed.html')
+    r=session["responses"]
+    return render_template('/completed.html', r=r)
 
